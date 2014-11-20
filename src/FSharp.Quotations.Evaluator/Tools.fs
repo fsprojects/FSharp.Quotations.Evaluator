d9b0f09283c8caa4d1470d97e22953f7d7f6baa9
Paul Westcott <p...w.e.s.t.c.o.t.t@gmail.com>
2014-11-20 20:50:19 +1100

    Added tuple creator
    For unrestricted number of type parameters. Not currently wired in, but
    tests added and working.

---
+++ src/FSharp.Quotations.Evaluator/Tools.fs
@@ -0,0 +1,36 @@
+﻿module FSharp.Quotations.Evaluator.Tools
+
+open System.Linq.Expressions
+open System
+
+let createTuple<'a> types =
+    let tuple = typedefof<'a>.MakeGenericType types
+    let ``constructor`` = tuple.GetConstructor types
+    let callConstructor (x:seq<Expression>) = 
+        Expression.New (``constructor``, x) :> Expression
+    tuple, callConstructor
+
+let rec createGenericTupleType types =
+    match types with
+    | [||]                      -> typeof<Unit>, (fun _ -> Expression.Constant(null, typeof<unit>) :> Expression)
+    | [|t1|]                    -> createTuple<Tuple<_>>             types
+    | [|t1;t2|]                 -> createTuple<Tuple<_,_>>           types
+    | [|t1;t2;t3|]              -> createTuple<Tuple<_,_,_>>         types
+    | [|t1;t2;t3;t4|]           -> createTuple<Tuple<_,_,_,_>>       types
+    | [|t1;t2;t3;t4;t5|]        -> createTuple<Tuple<_,_,_,_,_>>     types
+    | [|t1;t2;t3;t4;t5;t6|]     -> createTuple<Tuple<_,_,_,_,_,_>>   types
+    | [|t1;t2;t3;t4;t5;t6;t7|]  -> createTuple<Tuple<_,_,_,_,_,_,_>> types
+    | _ ->
+        let slice = types.[7..]
+        let innerTuple, create = createGenericTupleType slice
+        let sliceTypes = [| yield! types.[0..6]; yield innerTuple |]
+        let tuple = typedefof<Tuple<_,_,_,_,_,_,_,_>>.MakeGenericType sliceTypes
+        let ``constructor`` = tuple.GetConstructor sliceTypes
+        let callConstructor (x:seq<Expression>) =
+            let args = [|
+                yield! Seq.take 7 x 
+                yield create (Seq.skip 7 x) |]
+            Expression.New(``constructor``, args) :> Expression
+        tuple, callConstructor
+
+
