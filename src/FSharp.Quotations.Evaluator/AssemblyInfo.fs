0e334366eeb885dba2c5a9302826bb0ecc5760ad
Paul Westcott <p...w.e.s.t.c.o.t.t@gmail.com>
2014-11-23 20:54:55 +1100

    Bump version to 1.0.4

--- src/FSharp.Quotations.Evaluator/AssemblyInfo.fs
+++ src/FSharp.Quotations.Evaluator/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Quotations.Evaluator")>]
 [<assembly: AssemblyProductAttribute("FSharp.Quotations.Evaluator")>]
 [<assembly: AssemblyDescriptionAttribute("A quotations evaluator for F# based on LINQ expression tree compilation")>]
-[<assembly: AssemblyVersionAttribute("1.0.3")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.3")>]
+[<assembly: AssemblyVersionAttribute("1.0.4")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.4")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.3"
+    let [<Literal>] Version = "1.0.4"
